a53d2cc463351a4ca4bfb22d9f2f88d76b01a78a
vivitc <vivitc@gmail.com>
2014-03-19 19:33:17 -0400

    Adding route to update scripts steps.

--- prioritizer.py
+++ prioritizer.py
@@ -9,6 +9,13 @@ def outgoing_message_router():
     steps_cache = StepsCache()
     return "Hello, World!"
 
+@app.route("/update_script_steps")
+def update_script_steps():
+    steps_cache = StepsCache("username", "password")
+    client = steps_cache.get_redis_client()
+    steps_cache.delete_script_steps_data(client)
+    steps_cache.add_script_steps_data(client)
+    return "Steps script in cache successfully updated"
 
 if __name__ == "__main__":
 
